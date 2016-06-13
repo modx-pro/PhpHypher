b1a9ed53d7fd795c3b0fc4685d08181232710825
Alexey Ch <faitnomor@gmail.com>
2016-06-13 10:42:21 +0500

    Add some regex optimisation Version 1.0.2-pl

--- _build/build.config.php
+++ _build/build.config.php
@@ -1,17 +1,27 @@
 <?php
-/**
- * Define the MODX path constants necessary for core installation
- *
- * @package bannery
- * @subpackage build
- */
-define('MODX_BASE_PATH', dirname(dirname(dirname(__FILE__))) . '/');
+
+// define package
+define('PKG_NAME', 'PhpHypher');
+define('PKG_NAME_LOWER', strtolower(PKG_NAME));
+define('PKG_VERSION', '1.0.2');
+define('PKG_RELEASE', 'pl');
+define('PKG_AUTO_INSTALL', true);
+define('PKG_NAMESPACE_PATH', '{core_path}components/' . PKG_NAME_LOWER . '/');
+
+if (isset($_SERVER['MODX_BASE_PATH'])) {
+    define('MODX_BASE_PATH', $_SERVER['MODX_BASE_PATH']);
+} elseif (file_exists(dirname(dirname(dirname(__FILE__))) . '/core')) {
+    define('MODX_BASE_PATH', dirname(dirname(dirname(__FILE__))) . '/');
+} else {
+    define('MODX_BASE_PATH', dirname(dirname(dirname(dirname(__FILE__)))) . '/');
+}
+
 define('MODX_CORE_PATH', MODX_BASE_PATH . 'core/');
 define('MODX_MANAGER_PATH', MODX_BASE_PATH . 'manager/');
 define('MODX_CONNECTORS_PATH', MODX_BASE_PATH . 'connectors/');
 define('MODX_ASSETS_PATH', MODX_BASE_PATH . 'assets/');
 
-define('MODX_BASE_URL','/');
+define('MODX_BASE_URL', '/');
 define('MODX_CORE_URL', MODX_BASE_URL . 'core/');
 define('MODX_MANAGER_URL', MODX_BASE_URL . 'manager/');
 define('MODX_CONNECTORS_URL', MODX_BASE_URL . 'connectors/');
